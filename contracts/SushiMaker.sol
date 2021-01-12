aa33fe5cf340518610fc2dbed033707533902b90
Anton Davydov <fetsorn@gmail.com>
2021-01-12 03:13:24 +0300

    feat: change constant function formula

--- contracts/SushiMaker.sol
+++ contracts/SushiMaker.sol
@@ -63,7 +63,7 @@ contract SushiMaker {
         address token0 = pair.token0();
         (uint reserveIn, uint reserveOut) = token0 == token ? (reserve0, reserve1) : (reserve1, reserve0);
         uint amountInWithFee = amountIn.mul(997);
-        uint amountOut = amountInWithFee.mul(reserveOut) / reserveIn.mul(1000).add(amountInWithFee);
+        uint amountOut = amountInWithFee.mul(sqrt(reserveOut)) / sqrt(reserveIn).mul(1000).add(amountInWithFee);
         (uint amount0Out, uint amount1Out) = token0 == token ? (uint(0), amountOut) : (amountOut, uint(0));
         _safeTransfer(token, address(pair), amountIn);
         pair.swap(amount0Out, amount1Out, factory.getPair(weth, sushi), new bytes(0));
@@ -76,8 +76,8 @@ contract SushiMaker {
         address token0 = pair.token0();
         (uint reserveIn, uint reserveOut) = token0 == weth ? (reserve0, reserve1) : (reserve1, reserve0);
         uint amountInWithFee = amountIn.mul(997);
-        uint numerator = amountInWithFee.mul(reserveOut);
-        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+        uint numerator = amountInWithFee.mul(sqrt(reserveOut));
+        uint denominator = sqrt(reserveIn).mul(1000).add(amountInWithFee);
         uint amountOut = numerator / denominator;
         (uint amount0Out, uint amount1Out) = token0 == weth ? (uint(0), amountOut) : (amountOut, uint(0));
         pair.swap(amount0Out, amount1Out, bar, new bytes(0));
@@ -86,4 +86,35 @@ contract SushiMaker {
     function _safeTransfer(address token, address to, uint256 amount) internal {
         IERC20(token).safeTransfer(to, amount);
     }
+
+  /**
+   * Calculate sqrt (x) rounding down, where x is unsigned 256-bit integer
+   * number.
+   *
+   * @param x unsigned 256-bit integer number
+   * @return unsigned 256-bit integer number
+   */
+  function sqrt (uint256 x) private pure returns (uint256) {
+    if (x == 0) return 0;
+    else {
+      uint256 xx = x;
+      uint256 r = 1;
+      if (xx >= 0x100000000000000000000000000000000) { xx >>= 128; r <<= 64; }
+      if (xx >= 0x10000000000000000) { xx >>= 64; r <<= 32; }
+      if (xx >= 0x100000000) { xx >>= 32; r <<= 16; }
+      if (xx >= 0x10000) { xx >>= 16; r <<= 8; }
+      if (xx >= 0x100) { xx >>= 8; r <<= 4; }
+      if (xx >= 0x10) { xx >>= 4; r <<= 2; }
+      if (xx >= 0x8) { r <<= 1; }
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1;
+      r = (r + x / r) >> 1; // Seven iterations should be enough
+      return uint256 (x / r);
+    }
+  }
+
 }
